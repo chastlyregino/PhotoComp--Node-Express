a3c3e0d33b2d1aaf4498e494385b1bafd827cd9c
Chastly Regino <chastly_regino@icloud.com>
2025-03-25 16:19:42 -0500

    created org and member endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { authRouter } from './controllers/authController';
+import { orgController } from './controller/orgController';
 import { errorHandler } from './middleware/errorHandler';
 
 // Load environment variables
@@ -14,23 +15,23 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Error handling middleware must be used after all routes
+app.use(errorHandler);
+
 // Routes
 app.use('/api/auth', authRouter);
+app.use(`/organizations`, orgController);
 
 // Default route
 app.get('/', (req, res) => {
   res.send('PhotoComp API is running');
 });
 
-// 404 handler - must be before the error handler
-app.use((req, res, next) => {
+app.all((req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 });
-
-// Error handling middleware must be used after all routes
-app.use(errorHandler);
 
 // Start server
 app.listen(PORT, () => {
