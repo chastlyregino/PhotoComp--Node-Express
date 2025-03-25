8a4f7c0fc3dc159fe32727f1696d8dfce552399f
Bogdan Shmat <bogdan.shmat@wsu.edu>
2025-03-25 16:43:03 -0700

    remove junk files

--- src/models/Organization.ts
+++
@@ -1,9 +0,0 @@
-export interface Organization {
-    id: string;
-    name: string;
-    description: string;
-    logo?: string;
-    createdAt: Date;
-    updatedAt: Date;
-    createdBy: string; // User ID
-  }
