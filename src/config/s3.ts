df2afec2fd4398c299b2ac81d9c08891649e22a6
Bogdan Shmat <bogdan.shmat@wsu.edu>
2025-04-03 10:50:50 -0700

    s3 config

---
+++ src/config/s3.ts
@@ -0,0 +1,11 @@
+import { S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const s3Client = new S3Client({ region: process.env.REGION as string });
+
+export const BUCKET_NAME = process.env.S3_BUCKET_NAME as string;
+
+export { s3Client, getSignedUrl };
