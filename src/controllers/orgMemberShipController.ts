efabed5b70ccaf735c5545d10b422bfecf2f778d
Carlos Tapia <Tapiacarlos32@gmail.com>
2025-04-06 14:09:20 -0500

    Add approve and deny org requests while org has events

--- src/controllers/orgMemberShipController.ts
+++ src/controllers/orgMemberShipController.ts
@@ -1,16 +1,17 @@
-// src/controllers/orgMembershipController.ts
-
 import { Request, Response, Router, NextFunction } from 'express';
 import { OrgMembershipService } from '../services/orgMembershipService';
 import { validateUserID } from './orgController';
-// import { checkOrgAdmin } from "../middleware/OrgMiddleware";
+import { checkOrgAdmin } from '../middleware/orgMiddleware';
+import { OrganizationMembershipRequest } from '../models/Organizations';
+import { UserService } from '@/services/userService';
 
 const orgMembershipService = new OrgMembershipService();
+const userService = new UserService();
 export const orgMembershipRouter = Router();
 
 /**
  * Apply to join an organization
- * @route POST /organizations/:id/apply
+ * @route POST /organizations/:id
  */
 orgMembershipRouter.post(
     '/:id',
@@ -44,100 +45,98 @@ orgMembershipRouter.post(
  * Get pending membership requests for an organization
  * @route GET /organizations/:id/requests
  */
-// orgMembershipRouter.get(
-//     '/:id/requests',
-//     validateUserID,
-//     checkOrgAdmin,
-//     async (req: Request, res: Response, next: NextFunction) => {
-//         try {
-//             const orgName: string = req.params.id;
-//             
-//             const requests = await orgMembershipService.getPendingRequests(orgName);
-//             
-//             // If there are requests, get user details for each
-//             const requestsWithUserDetails = await Promise.all(
-//                 requests.map(async (request) => {
-//                     const user = await orgMembershipService.getUserById(request.userId);
-//                     return {
-//                         ...request,
-//                         userDetails: user ? {
-//                             id: user.id,
-//                             email: user.email,
-//                             firstName: user.firstName,
-//                             lastName: user.lastName
-//                         } : null
-//                     };
-//                 })
-//             );
-//
-//             return res.status(200).json({
-//                 status: 'success',
-//                 data: {
-//                     requests: requestsWithUserDetails
-//                 }
-//             });
-//         } catch (error) {
-//             next(error);
-//         }
-//     }
-// );
+orgMembershipRouter.get(
+    '/:id/requests',
+    validateUserID,
+    checkOrgAdmin,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const orgName: string = req.params.id;
+            const requests: OrganizationMembershipRequest[] = await orgMembershipService.getPendingRequests(orgName);
+            // If there are requests, get user details for each
+            const requestsWithUserDetails = await Promise.all(
+                requests.map(async (request) => {
+                    const user = await userService.getUserById(request.userId);
+                    return {
+                        ...request,
+                        userDetails: user ? {
+                            id: user.id,
+                            email: user.email,
+                            firstName: user.firstName,
+                            lastName: user.lastName
+                        } : null
+                    };
+                })
+            );
+
+            return res.status(200).json({
+                status: 'success',
+                data: {
+                    requests: requestsWithUserDetails
+                }
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
 
 /**
  * Approve a membership request
- * @route POST /organizations/:id/requests/:userId/approve
+ * @route PUT /organizations/:id/requests/:userId
  */
-// orgMembershipRouter.post(
-//     '/:id/requests/:userId/approve',
-//     validateUserID,
-//     checkOrgAdmin,
-//     async (req: Request, res: Response, next: NextFunction) => {
-//         try {
-//             const orgName: string = req.params.id;
-//             const userId: string = req.params.userId;
-//
-//             const result = await orgMembershipService.approveRequest(
-//                 orgName,
-//                 userId
-//             );
-//
-//             return res.status(200).json({
-//                 status: 'success',
-//                 message: 'Membership request approved',
-//                 data: result
-//             });
-//         } catch (error) {
-//             next(error);
-//         }
-//     }
-// );
+orgMembershipRouter.put(
+    '/:id/requests/:userId',
+    validateUserID,
+    checkOrgAdmin,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const orgName: string = req.params.id;
+            const userId: string = req.params.userId;
+
+            const result = await orgMembershipService.approveRequest(
+                orgName,
+                userId
+            );
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'Membership request approved',
+                data: result
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
 
 /**
  * Deny a membership request
- * @route POST /organizations/:id/requests/:userId/deny
+ * @route POST /organizations/:id/requests/:userId
  */
-// orgMembershipRouter.post(
-//     '/:id/requests/:userId/deny',
-//     validateUserID,
-//     checkOrgAdmin,
-//     async (req: Request, res: Response, next: NextFunction) => {
-//         try {
-//             const orgName: string = req.params.id;
-//             const userId: string = req.params.userId;
-//
-//             const result = await orgMembershipService.denyRequest(
-//                 orgName,
-//                 userId
-//             );
-//
-//             return res.status(200).json({
-//                 status: 'success',
-//                 message: 'Membership request denied',
-//                 data: {
-//                     request: result
-//                 }
-//             });
-//         } catch (error) {
-//             next(error);
-//         }
-//     }
-// );
+orgMembershipRouter.delete(
+    '/:id/requests/:userId',
+    validateUserID,
+    checkOrgAdmin,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const orgName: string = req.params.id;
+            const userId: string = req.params.userId;
+
+            const result = await orgMembershipService.denyRequest(
+                orgName,
+                userId
+            );
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'Membership request denied',
+                data: {
+                    request: result
+                }
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
