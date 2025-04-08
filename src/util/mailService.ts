bdb36aa4b7a313c0162aeb35156ee8347b94d550
Chastly Regino <chastly_regino@icloud.com>
2025-04-08 16:09:38 -0500

    Members can receive an email notification when an event of their org is created

--- src/util/mailService.ts
+++ src/util/mailService.ts
@@ -1,30 +1,8 @@
 import nodemailer from 'nodemailer';
-//import html from './emailTemplate.html'
-//const hbs = require('nodemailer-express-handlebars')
 import { logger } from './logger';
 import dotenv from 'dotenv';
-import path from 'path';
 
 dotenv.config();
-
-// const handlebarOptions = {
-//     viewEngine: {
-//         partialsDir: path.resolve('../views/'),
-//         defaultLayout: false,
-//     },
-//     viewPath: path.resolve('../views/'),
-// };
-
-const handlebarOptions = {
-    viewEngine: {
-        extName: '.hbs',
-        partialsDir: '../views/',
-        layoutsDir: '../views/',
-        defaultLayout: 'email.body.hbs',
-    },
-    viewPath: '../views/',
-    extName: '.hbs',
-};
 
 const sendMail = async (to: string, subject: string, message: string, header: string) => {
     const transporter = nodemailer.createTransport({
@@ -34,8 +12,6 @@ const sendMail = async (to: string, subject: string, message: string, header: st
             pass: process.env.MAIL_PASSWORD,
         },
     });
-
-    //transporter.use('compile', hbs(handlebarOptions));
 
     const mailOptions = {
         from: `PhotoComp Admin: ${process.env.MAIL_USERNAME}`,
@@ -54,16 +30,6 @@ const sendMail = async (to: string, subject: string, message: string, header: st
             logger.info('Email sent: ' + info.response);
         }
     });
-
-    // const mailOptions = {
-    //     from: '"My Company" <my@company.com>', // sender address
-    //     template: "email", // the name of the template file, i.e., email.handlebars
-    //     to: user.email,
-    //     subject: `Welcome to My Company, ${user.name}`,
-    //     context: {
-    //       name: user.name,
-    //       company: 'my company'
-    //     },
 };
 
 export { sendMail };
