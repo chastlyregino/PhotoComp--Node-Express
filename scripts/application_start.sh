d7d7fb1d65654f23bfad088fc285d3d76eae861a
Chastly Regino <chastly_regino@icloud.com>
2025-04-19 19:29:57 -0500

    env file

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/PhotoComp--Node-Express
+
+export TABLE_NAME=PhotoCompTable
+export JWT_SECRET=your_jwt_secret_here
+export EXPIRES_IN=1h
+export REGION=us-east-1
+export S3_BUCKET_NAME=photo-comp-s3-bucket
+export MAIL_HOST=gmail
+export MAIL_USERNAME=chasregino@gmail.com
+export MAIL_PASSWORD=gibprzfucmvjafef
+export PORT=3000
+
 node dist/index.js
